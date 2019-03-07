5d79f66b771c06a9bf2fee3635975976a643e107
fermartz <fernandomvc@gmail.com>
2019-03-07 07:57:44 -0600

    add docs folder

---
+++ docs/precache-manifest.cd518c2ff528f1bfaf346692732efde0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "640b137d2f2530ce2a52",
+    "url": "/static/js/main.640b137d.chunk.js"
+  },
+  {
+    "revision": "680a2c04a3643f76f3ae",
+    "url": "/static/js/1.680a2c04.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e6a9410360cd0a5e2cb27459e956c860",
+    "url": "/index.html"
+  }
+];
