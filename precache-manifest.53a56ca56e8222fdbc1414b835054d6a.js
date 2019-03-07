e65b8ce4b1f8f49653ab4568477435bfaa599dd7
ferMartz <fernandomvc@gmail.com>
2019-03-07 17:50:09 -0600

    Updates

---
+++ precache-manifest.53a56ca56e8222fdbc1414b835054d6a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b44680ce2bcceb33d5a7",
+    "url": "/one/static/js/runtime~main.b44680ce.js"
+  },
+  {
+    "revision": "ba92eeff79f2ae7991af",
+    "url": "/one/static/js/main.ba92eeff.chunk.js"
+  },
+  {
+    "revision": "1c8711f0aed2089268ea",
+    "url": "/one/static/js/1.1c8711f0.chunk.js"
+  },
+  {
+    "revision": "f91e5bf6667bbe1f07bb111f8f8f6f03",
+    "url": "/one/index.html"
+  }
+];
